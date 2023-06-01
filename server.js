84a69e5f9617ac92f7c0e0989adc7042ef02edf2
Smith1 <seth20smith@gmail.com>
2023-06-01 15:01:05 -0700

    fixed button

--- server.js
+++ server.js
@@ -1,56 +1,54 @@
 const express = require('express');
-const path = require('path')
+const path = require('path');
 const app = express();
 const db = require('./db/db.json');
 const fs = require('fs')
 const uuid = require('uuidv1');
 //const { json } = require('express');
-
 // const = require db.json, urlroutes, fs
+const PORT = process.env.PORT || 3001
 
-  const PORT = process.env.PORT || 3001
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: true }));
-  app.use(express.static('public'));
+// app use url, api
 
-  // app use url, api
- 
-  app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-  });
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/index.html'));
+});
 
-  app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
-  });
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
 
 
 app.get('/api/notes', (req, res) => {
-    const dbNotes = fs.readFileSync('./db/db.json','utf-8')
-    const notes = JSON.parse(dbNotes|| []) 
-    res.json(notes)
-  });
+  const dbNotes = fs.readFileSync('./db/db.json', 'utf-8')
+  const notes = JSON.parse(dbNotes || [])
+  res.json(notes)
+});
 
 
-app.post ('/api/notes', (req, res) => {
-    const title = req.body.title
-    const text = req.body.text
+app.post('/api/notes', (req, res) => {
+  const title = req.body.title
+  const text = req.body.text
 
-  if (title, text) 
-  {const newNote = {
-    title, text, id: uuid ()
-  }
+  if (title, text) {
+    const newNote = {
+      title, text, id: uuid()
+    }
     const response = {
-      status: "success", 
+      status: "success",
       body: newNote
     }
-    try{
-        const notes = fs.readFileSync('./db/db.json','utf-8')
-        const notesO = JSON.parse(notes)
-        notesO.push(newNote)
-        fs.writeFileSync('./db/db.json', JSON.stringify(notesO))
+    try {
+      const notes = fs.readFileSync('./db/db.json', 'utf-8')
+      const notesO = JSON.parse(notes)
+      notesO.push(newNote)
+      fs.writeFileSync('./db/db.json', JSON.stringify(notesO))
 
-    } catch(error) {
+    } catch (error) {
       console.log(error)
       res.json(error)
     }
@@ -60,14 +58,18 @@ app.post ('/api/notes', (req, res) => {
 
 app.delete('/api/notes/:id', (req, res) => {
   const noteId = req.params.id;
+  console.log("Deleting note with ID: ", noteId);  // Added console log
+
   let notes = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
   notes = notes.filter((note) => note.id !== noteId);
+
+  console.log("Updated notes: ", notes);  // Added console log
+
   fs.writeFileSync('./db/db.json', JSON.stringify(notes));
   res.json(notes);
 });
 
 
-  app.listen(PORT, () => {
-    console.log (`Example app listening at http://localhost: ${PORT}`);
-  });
-  
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost: ${PORT}`);
+});
